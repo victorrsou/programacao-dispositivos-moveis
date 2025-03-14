df217a43d62fcaf94c891eec3f6363df47cfb6dc
Victor Souza <rochadesouzavictor@gmail.com>
2025-03-14 20:37:02 -0300

    Adicionando arquivos da pratica03

---
+++ praticas/pratica03/App.js
@@ -0,0 +1,12 @@
+import GalleryScreen from "./screens/GalleryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ListScreen from "./screens/ListScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+function App() {
+  return (
+    <ProfileScreen></ProfileScreen>
+  )
+}
+
+export default App;
---
+++ praticas/pratica03/screens/ListScreen.js
@@ -0,0 +1,70 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function ListScreen() {
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: "#FFFBFE",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+        },
+        appBar: {
+            backgroundColor: "#6750A4",
+            height: 64,
+            paddingLeft: 16,
+            paddingRight: 16,
+            justifyContent: "center",
+            marginBottom: 8,
+        },
+        item: {
+            backgroundColor: "#FEF7FF",
+            height: 72,
+            borderBottomWidth: 1,
+            borderBottomColor: "#79747E",
+            paddingTop: 8,
+            paddingLeft: 16,
+            paddingRight: 16,
+            paddingBottom: 8,
+        },
+        itemTitle: {
+            color: "#49454F",
+            fontSize: 14,
+            fontWeight: "400",
+            lineHeight: 20,
+        },
+        itemSubtitle: {
+            color: "#49454F",
+            fontSize: 14,
+            fontWeight: "400",
+            lineHeight: 20,
+        },
+
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.appBar}>
+                <Text style={styles.appTitle}>Listagem de Itens</Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Título do Item 1</Text>
+                <Text style={styles.itemSubtitle}>Descrição breve do item 1.</Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Título do Item 2</Text>
+                <Text style={styles.itemSubtitle}>Descrição breve do item 2.</Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Título do Item 3</Text>
+                <Text style={styles.itemSubtitle}>Descrição breve do item 3.</Text>
+            </View>
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Título do Item 4</Text>
+                <Text style={styles.itemSubtitle}>Descrição breve do item 4.</Text>
+            </View>
+        </View>
+    );
+}
+
+export default ListScreen;
