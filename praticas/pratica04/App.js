cb585c7ffa6fee2b06b0b405d5e7ce6fdb440f57
Victor Souza <rochadesouzavictor@gmail.com>
2025-03-27 18:31:03 -0300

    Adicionando arquivos da pratica04

---
+++ praticas/pratica04/App.js
@@ -0,0 +1,10 @@
+import ContactScreen from "./screens/ContactScreen";
+import DetailScreen from "./screens/DetailScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
+function App() {
+  return <DetailScreen/>
+}
+
+export default App;
